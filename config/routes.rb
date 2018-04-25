38b7ae1e44ecbda62105f797e39b2da8ed109fd6
Dainel Vera <arubinofaux@gmail.com>
2018-04-25 15:59:12 -0400

    devise links

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'page/home'
   resources :blocks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
